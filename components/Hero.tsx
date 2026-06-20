abc1072d8a090b6178fe439326079dd209caf3fd
Johnny Cage <thespammingbott@gmail.com>
2026-06-20 22:20:03 +0100

    add gradients to hero header and nav text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
     return (
         <section className="grid justify-center align-center mt-15">
-            <h2 className="text-5xl">Shorten your loooong links:)</h2>
+            <h2 className="text-6xl font-bold bg-[linear-gradient(90deg,#144EE3_0%,#EB568E_19%,#A353AA_64%,#144EE3_100%)] bg-clip-text text-transparent">Shorten your loooong links:)</h2>
             <p>Linkly is an efficient and easy-to-use URL shortening service that streamlines your online experience.</p>
             <div className="flex gap-2">
                 <input type="text"  placeholder="Enter the link here" className="border rounded-xl"/>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <header>
             <nav>
-                <p className="text-3xl py-5 pl-5">uRL shortener</p>
+                <p className="text-3xl py-5 pl-5 font-bold bg-[radial-gradient(ellipse_at_5.56%_79.01%,#EB568E_0%,#144EE3_100%)] bg-clip-text text-transparent">uRL shortener</p>
             </nav>
         </header>
     )
